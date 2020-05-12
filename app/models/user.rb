242cac8ec079ace047074f27526ecc1738a99f16
yuu <y.binsaki@kageyama-co.jp>
2020-05-12 15:09:54 +0900

    Update seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+    enum role: { Admin: 0, Normal: 1 }
 end
--- db/migrate/20200512052658_create_users.rb
+++ db/migrate/20200512052658_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.integer :role
+      t.role :role
       t.string :password
       t.integer :user_id
 
